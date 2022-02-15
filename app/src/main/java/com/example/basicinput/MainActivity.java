36d178d777a6785231ef5c3651cde955b0b7049d
Tiago Almeida <thyagoeap@gmail.com>
2022-02-15 09:05:39 -0300

    add listner callback

--- app/src/main/java/com/example/basicinput/MainActivity.java
+++ app/src/main/java/com/example/basicinput/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -13,7 +15,8 @@ import com.google.android.material.textfield.TextInputLayout;
 public class MainActivity extends AppCompatActivity {
     private TextInputEditText nameInputLayout, emailInputLayout;
     private CheckBox redCheckBox, greenCheckBox, blueCheckBox;
-    private TextView nameTextView, emailTextView, colorsTextView;
+    private TextView nameTextView, emailTextView, colorsTextView, sexText;
+    private RadioGroup radioGroupSex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
         redCheckBox = findViewById(R.id.red);
         greenCheckBox = findViewById(R.id.green);
         blueCheckBox = findViewById(R.id.blue);
+        radioGroupSex = findViewById(R.id.radioGroupSex);
+        sexText = findViewById(R.id.sexText);
+
+        handleRadioButton();
     }
 
     private void handleCheckBox () {
@@ -45,12 +52,25 @@ public class MainActivity extends AppCompatActivity {
         colorsTextView.setText(selectedBox);
     }
 
+    private void handleRadioButton () {
+        radioGroupSex.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId != -1) {
+                    RadioButton radioButton = findViewById(checkedId);
+                    sexText.setText("Sexo: "+radioButton.getText().toString());
+                }
+            }
+        });
+    }
+
     public void onSubmit(View view) {
         String nameValue = nameInputLayout.getText().toString();
         nameTextView.setText(nameValue);
         String emailValue = emailInputLayout.getText().toString();
         emailTextView.setText(emailValue);
         handleCheckBox();
+        handleRadioButton();
     }
 
     public void clearText(View view) {
@@ -62,5 +82,7 @@ public class MainActivity extends AppCompatActivity {
         redCheckBox.setChecked(false);
         greenCheckBox.setChecked(false);
         blueCheckBox.setChecked(false);
+        radioGroupSex.clearCheck();
+        sexText.setText("");
     }
 }
